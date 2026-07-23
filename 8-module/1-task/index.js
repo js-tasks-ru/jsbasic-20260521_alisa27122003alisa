6c7a3e27742a8f4b9dc42ff726624201f061befc
Alisa Nechaeva <ne-chai-va@yandex.ru>
2026-07-23 18:40:43 +0300

    Fix updatePosition() method for cart icon component

--- 8-module/1-task/index.js
+++ 8-module/1-task/index.js
@@ -18,7 +18,9 @@ export default class CartIcon {
       this.elem.innerHTML = `
         <div class="cart-icon__inner">
           <span class="cart-icon__count">${cart.getTotalCount()}</span>
-          <span class="cart-icon__price">€${cart.getTotalPrice().toFixed(2)}</span>
+          <span class="cart-icon__price">€${cart
+            .getTotalPrice()
+            .toFixed(2)}</span>
         </div>`;
 
       this.updatePosition();
@@ -29,7 +31,7 @@ export default class CartIcon {
         () => {
           this.elem.classList.remove("shake");
         },
-        { once: true },
+        { once: true }
       );
     } else {
       this.elem.classList.remove("cart-icon_visible");
@@ -42,39 +44,45 @@ export default class CartIcon {
   }
 
   updatePosition() {
-    let initialTopCoord =
-      this.elem.getBoundingClientRect().top + window.pageYOffset;
+    const isMobile = document.documentElement.clientWidth <= 767;
 
-    let isMobile = document.documentElement.clientWidth <= 767;
-
-    if (document.documentElement.clientWidth <= 767) {
+    if (isMobile) {
       Object.assign(this.elem.style, {
         position: "",
         top: "",
+        right: "",
         left: "",
         zIndex: "",
       });
+
+      return;
     }
 
-    if (window.pageYOffset > initialTopCoord) {
-      let leftIndent =
+    if (this.initialTopCoord === undefined) {
+      this.initialTopCoord =
+        this.elem.getBoundingClientRect().top + window.pageYOffset;
+    }
+
+    if (window.pageYOffset > this.initialTopCoord) {
+      const leftIndent =
         Math.min(
           document.querySelector(".container").getBoundingClientRect().right +
             20,
-          document.documentElement.clientWidth - this.elem.offsetWidth - 10,
+          document.documentElement.clientWidth - this.elem.offsetWidth - 10
         ) + "px";
 
       Object.assign(this.elem.style, {
         position: "fixed",
         top: "50px",
-        zIndex: 10,
-        right: "10px",
+        right: "auto",
         left: leftIndent,
+        zIndex: "10",
       });
     } else {
       Object.assign(this.elem.style, {
         position: "",
         top: "",
+        right: "",
         left: "",
         zIndex: "",
       });
